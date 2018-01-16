d93eb0101a3ad594bb89de32e37946cc46bd5c4b
hassan.reyes <hassan.reyes@gmail.com>
2018-01-16 12:31:07 -0600

    First commit

--- Startup.cs
+++ Startup.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,9 +7,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using Microsoft.AspNetCore.StaticFiles;
 using MyWebAPI.Entities;
 using MyWebAPI.Repositories;
 
